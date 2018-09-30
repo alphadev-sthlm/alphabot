42b8c28efc8a59970023b3973ab4c757fc10d343
Leon Hennings <leon.hennings@gmail.com>
2018-09-30 22:41:44 +0200

    Initial bot setup

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # alphabot
 Our slackbot
+
+## Setup
+
+Get pip
+
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    sudo python get-pip.py
+
+Get virtualenv
+
+    sudo pip install virtualenv
+    virtualenv env
+    source env/bin/activate
+    pip install -r requirements.txt
+
+
+Create .env file
+
+    SLACK_API_TOKEN=XXXX
+    APP_ENV=development
+
+Run locally
+
+    python alphabot.py
+
+Deploy (will create lambda with -dev suffix)
+
+    zappa deploy
+    zappa update
+    zappa tail
+
+## Common problems
+
+If you get inexplicable zappa error 
+
+    ImportError: bad magic number in 'config': b'\x03\xf3\r\n'
+
+Solution
+
+    find . -name \*.pyc -delete
---
+++ app/utils/slackhelper.py
@@ -0,0 +1,49 @@
+from slackclient import SlackClient
+from config import get_env
+
+
+class SlackHelper:
+
+    def __init__(self):
+        self.slack_token = get_env('SLACK_API_TOKEN')
+        self.slack_client = SlackClient(self.slack_token)
+        self.slack_channel = get_env('SLACK_CHANNEL')
+
+        print(self.slack_token)
+        print(self.slack_channel)
+
+    def post_message(self, msg, recipient):
+        return self.slack_client.api_call(
+            "chat.postMessage",
+            channel=recipient,
+            text=msg,
+            as_user=True
+        )
+
+    def post_message_to_channel(self, msg):
+        return self.slack_client.api_call(
+            "chat.postMessage",
+            channel=self.slack_channel,
+            text=msg,
+            username='alphabot',
+            parse='full',
+            as_user=False
+        )
+
+    def file_upload(self, file_content, file_name, file_type, title=None, ):
+        return self.slack_client.api_call(
+            "files.upload",
+            channels=self.slack_channel,
+            content=file_content,
+            filename=file_name,
+            filetype=file_type,
+            initial_comment='{} Log File'.format(file_name),
+            title=title
+        )
+
+    def user_info(self, uid):
+        return self.slack_client.api_call(
+            "users.info",
+            user=uid,
+            token=self.slack_token
+        )
